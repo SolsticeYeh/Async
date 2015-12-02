422d792842a59ed8599fcdc7570b98b08fdd9e6d
nealbity <yezhou2@outlook.com>
2015-12-02 12:55:11 +0800

    Async

---
+++ Async/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int a = 0; a < 5; a++)
+            {
+                mAsync();
+                Console.WriteLine("@");
+                Thread.Sleep(1000);
+                Console.WriteLine("@");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public static async Task<int> mAsync()
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                Console.Write("$");
+                return 0;
+            });
+        }
+
+    }
+}
